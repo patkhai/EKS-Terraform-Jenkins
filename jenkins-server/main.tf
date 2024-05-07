9a5fb7069ddc2e6a8c93989cea5ac90b1900bea0
patkhai <patrick.phyo@gmail.com>
2024-05-06 17:25:44 -0700

    create jenkin server

---
+++ jenkins-server/main.tf
@@ -0,0 +1,89 @@
+data "aws_availability_zones" "azs" {}
+
+#Vpc
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "jenkins_vpc"
+  cidr = var.vpc_cidr
+
+  azs            = data.aws_availability_zones.azs.names
+
+  public_subnets = var.public_subnets
+
+  enable_dns_hostnames    = true
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "jenkins_vpc"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+  public_subnet_tags = {
+    Name = "jenkins_subnet"
+  }
+}
+
+#sg 
+
+module "sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "jenkins_sg"
+  description = "Security group for jenkins server"
+  vpc_id      = module.vpc.vpc_id
+
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      description = "HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "SSH"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  tags = {
+    Name = "jenkins_sg"
+  }
+}
+
+#ec2
+
+module "ec2_instance" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "jenkins_server"
+
+  instance_type               = var.instance_type
+  ami                         = "ami-04b70fa74e45c3917"
+  key_name                    = "swiggy"
+  monitoring                  = true
+  vpc_security_group_ids      = [module.sg.security_group_id]
+  availability_zone           = data.aws_availability_zones.azs.names[0]
+  subnet_id                   = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
+  user_data                   = file("jenkins-install.sh")
+
+
+  tags = {
+    Name        = "jenkins_server",
+    Terraform   = "true",
+    Environment = "dev"
+  }
+}
